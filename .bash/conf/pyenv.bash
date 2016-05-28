292c74e08195c549c77cc8802f3d837361eab495
itkr <itkrst@gmail.com>
2016-05-28 22:54:38 +0900

    Update pyenv.bash

--- .bash/conf/pyenv.bash
+++ .bash/conf/pyenv.bash
@@ -1,4 +1,5 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+export PYENV_ROOT=~/.pyenv
+#export PATH="$PYENV_ROOT/bin:$PATH"
+#if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
