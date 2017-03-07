a44b59be7d56c7655ba53b8fc3b6e3ee9cd0d5ef
itkr <itkrst@gmail.com>
2017-03-07 21:14:41 +0900

    Update dropbox settings

--- Scripts/link_dropbox.sh
+++ Scripts/link_dropbox.sh
@@ -1,11 +1,17 @@
 # Dropboxと連携するためのシンボリックリンクを設定する
 
-# Dropboxの場所
-DROPBOX_HOME="${HOME}/Dropbox\ \(個人\)"
+# TODO: Ansibleを使うようにする
 
-# iTunesライブラリ
-# ln -s ${DROPBOX_HOME}/Music/iTunes ${HOME}/Music/iTunes
+# Dropboxの場所
+DROPBOX_HOME="${HOME}/Dropbox"
+
+# iTunesライブラリ（母艦以外）
+# シンボリックリンクを使わずにiTunesの設定で操作できるが、バージョンの違いによってうまく行かなかったので追加
 ln -s ${DROPBOX_HOME}/Music/iTunes/iTunes\ Media/Music ${HOME}/Music/iTunes/iTunes\ Media/Music
 
+# iTunesライブラリ（母艦）
+# ln -s ${DROPBOX_HOME}/Music/iTunes ${HOME}/Music/iTunes
+
 # memoコマンドの保存先
+# シンボリックリンクではなく`memo config`コマンドでも変更できる
 ln -s ${DROPBOX_HOME}/Memo/_posts ${HOME}/.config/memo/_posts
