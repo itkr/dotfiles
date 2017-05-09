6b480fc0ee455dcadc6d92f96d8f1c7354dfaa91
itkr <itkrst@gmail.com>
2017-05-10 01:24:12 +0900

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,7 @@
 
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
+set -U fish_user_paths ~/.pyenv/bin $fish_user_paths
 
 # prompt
 
