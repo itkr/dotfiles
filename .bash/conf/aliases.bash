80d13094c99712f34395c43273c82cd4354457d3
itkr <itkrst@gmail.com>
2017-05-11 00:28:29 +0900

    Update bash conf

--- .bash/conf/aliases.bash
+++ .bash/conf/aliases.bash
@@ -2,3 +2,9 @@
 alias git='git stash list; git'
 alias fd='find ./ -type f -print | xargs grep'
 
+if (! which pbcopy > /dev/null); then
+    if which xsel > /dev/null; then
+        alias pbcopy='xsel -b'
+    fi
+fi
+
--- .bash/conf/pyenv.bash
+++ .bash/conf/pyenv.bash
@@ -1,5 +1,8 @@
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 export PYENV_ROOT=~/.pyenv
-#export PATH="$PYENV_ROOT/bin:$PATH"
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+export PATH="${PYENV_ROOT}/bin:${PATH}"
 
+# if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+# if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
