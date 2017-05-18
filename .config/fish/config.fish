c9b621eb2f923c478d8609485b2acdf10327cec1
itkr <itkrst@gmail.com>
2017-05-19 00:44:20 +0900

    Update fish settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,19 @@
-# function fish_prompt
-#     set_color $fish_color_cwd
-#     echo -n (prompt_pwd)
-#     set_color normal
-#     echo -n ' > '
-# end
-
+# ########################################
 # pyenv
+# ########################################
 
 set -U fish_user_paths ~/.pyenv/bin $fish_user_paths
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
+# ########################################
 # prompt
+# ########################################
 
 set -g fish_prompt_pwd_dir_length 0
 
 function fish_prompt
+    # set_color normal
     set_color $fish_color_cwd
     pyenv version | awk '{printf "(%s) ", $1;}'
     set_color 14968C
