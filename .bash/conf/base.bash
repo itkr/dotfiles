9814082833b826722bb37997dd1c6ae576a1aee7
itkr <itkrst@gmail.com>
2017-05-18 14:09:04 +0900

    ほとんど環境依存しないものは1ファイルにまとめる

---
+++ .bash/conf/base.bash
@@ -0,0 +1,46 @@
+# #### Alias ####
+
+alias git='git stash list; git'
+alias fd='find ./ -type f -print | xargs grep'
+alias fmt_python='~/.pyenv/versions/hipchat/bin/python ~/Scripts/format.py'
+
+# Linux pbcopy
+if (! which pbcopy > /dev/null); then
+    if which xsel > /dev/null; then
+        alias pbcopy='xsel -b'
+    fi
+fi
+
+# #### History ####
+
+export HISTSIZE=10000
+export HISTFILESIZE=$HISTSIZE
+
+function share_history {
+    history -a
+    history -c
+    history -r
+}
+PROMPT_COMMAND='share_history'
+shopt -u histappend
+
+# #### Prompt ####
+
+export PS1='\[\033[01;36m\]\w\[\033[00m\] \[\033[01;34m\]\$ \[\033[00m\]'
+
+#formattedGitBranch() {
+#    _branch=`git branch 2>/dev/null | grep '*' | tr -d ' *()'`
+#    test -n "$_branch" && echo -e "($_branch)"
+#}
+#PS1='\[\033[01;34m\]$(formattedGitBranch)\[\033[00m\]\w\$ '
+
+# #### Tool ####
+
+hiscopy() {
+    history | awk '{$1="";print $0;}' | sort | uniq | fzf --tac | awk '{printf "%s", $0;}' | pbcopy
+}
+
+gitb() {
+    git branch | peco | awk '{printf "%s", $NF;}' | pbcopy
+}
+
--- .bash/conf/tools.bash
+++
@@ -1,7 +0,0 @@
-hiscopy() {
-    history | awk '{$1="";print $0;}' | sort | uniq | fzf --tac | awk '{printf "%s", $0;}' | pbcopy
-}
-
-gitb() {
-    git branch | peco | awk '{printf "%s", $NF;}' | pbcopy
-}
