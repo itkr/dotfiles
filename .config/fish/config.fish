9d3147d32ed0029bccdf101f101fd30ce59fe9b1
itkr <itkrst@gmail.com>
2017-08-08 01:10:46 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -67,7 +67,7 @@ end
 # ########################################
 
 set -x GOPATH ~/.go
-set -x fish_user_paths $GOPATH/bin $fish_user_paths
+set -U fish_user_paths $GOPATH/bin $fish_user_paths
 
 # ########################################
 # Pyenv
