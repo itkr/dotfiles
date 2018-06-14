57ac16d9357353c6dd489c4a9da1de1e3a293e4a
itkr <1531860+itkr@users.noreply.github.com>
2018-06-15 00:40:35 +0900

    Update nodenv.bash

--- .bash/conf/nodenv.bash
+++ .bash/conf/nodenv.bash
@@ -1,8 +1,4 @@
 export NODENV_ROOT=~/.nodenv
 export PATH="${NODENV_ROOT}/bin:${PATH}"
 
-# if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-# if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-
 eval "$(nodenv init -)"
-eval "$(nodenv virtualenv-init -)"
