2917338d6d668819e7d1e814b50a6ec2c8d3bd3d
Shota Itakura <1531860+itkr@users.noreply.github.com>
2019-11-14 11:49:15 +0900

    Add link.sh

---
+++ link.sh
@@ -0,0 +1,33 @@
+# rm ~/.bash/conf/google-cloud-sdk.bash
+# rm ~/.bash/conf/homebrew.bash
+# rm ~/.bash/conf/nodenv.bash
+# rm ~/.bash/conf/pyenv.bash
+# rm ~/.bash_profile
+# rm ~/.bashrc
+# rm ~/.gitconfig
+# rm ~/.gitignore
+# rm ~/.gvimrc
+# rm ~/.ideavimrc
+# rm ~/.tmux.conf
+# rm ~/.vimrc
+# rm ~/.vrapperrc
+
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.bash/conf/google-cloud-sdk.bash ~/.bash/conf/google-cloud-sdk.bash
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.bash/conf/homebrew.bash ~/.bash/conf/homebrew.bash
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.bash/conf/nodenv.bash ~/.bash/conf/nodenv.bash
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.bash/conf/pyenv.bash ~/.bash/conf/pyenv.bash
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.bash_profile ~/.bash_profile
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.bashrc ~/.bashrc
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.gitconfig ~/.gitconfig
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.gitignore ~/.gitignore
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.gvimrc ~/.gvimrc
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.ideavimrc ~/.ideavimrc
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.tmux.conf ~/.tmux.conf
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.vimrc ~/.vimrc
+ln -s ~/.ghq/github.com/itkr/dotfiles/files/.vrapperrc ~/.vrapperrc
+
+# .vim/
+# .iterm2/
+# .matplotlib/
+# ln -s ~/Projects/github.com/itkr/dotfiles/files/.config/ ~/.config/
+# ln -s ~/Projects/github.com/itkr/dotfiles/files/.bash/ ~/.bash/
