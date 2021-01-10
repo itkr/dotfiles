19788ebc6c2466b01be071f744d123209cccbc59
Shota Itakura <1531860+itkr@users.noreply.github.com>
2021-01-11 00:47:59 +0900

    Update google-cloud-sdk conf

--- files/.zsh/conf/google-cloud-sdk.sh
+++ files/.zsh/conf/google-cloud-sdk.sh
@@ -7,9 +7,9 @@ fi
 
 if [ -x $CLOUD_SDK_PATH ]; then
     # The next line updates PATH for the Google Cloud SDK.
-    source $CLOUD_SDK_PATH/path.bash.inc
-    # The next line enables bash completion for gcloud.
-    source $CLOUD_SDK_PATH/completion.bash.inc
+    source $CLOUD_SDK_PATH/path.zsh.inc
+    # The next line enables zsh completion for gcloud.
+    source $CLOUD_SDK_PATH/completion.zsh.inc
 fi
 
 export CLOUDSDK_PYTHON="${HOME}/.pyenv/versions/global/bin/python"
