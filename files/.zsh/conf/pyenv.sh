89094d91621b0b0f803a715475607439f6838131
Shota Itakura <1531860+itkr@users.noreply.github.com>
2021-12-14 14:24:13 +0900

    Update pyenv

--- files/.zsh/conf/pyenv.sh
+++ files/.zsh/conf/pyenv.sh
@@ -1,8 +1,10 @@
 export PYENV_ROOT=~/.pyenv
 export PATH="${PYENV_ROOT}/bin:${PATH}"
 
+if which pyenv > /dev/null; then eval "$(pyenv init --path)"; fi
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
+# eval "$(pyenv init --path)"
 # eval "$(pyenv init -)"
 # eval "$(pyenv virtualenv-init -)"
