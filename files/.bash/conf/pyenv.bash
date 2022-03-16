94a8ca5ed47319f5dd8788c78dbb65094150fe19
Shota Itakura <1531860+itkr@users.noreply.github.com>
2022-03-17 00:13:14 +0900

    Update pyenv settings

--- files/.bash/conf/pyenv.bash
+++ files/.bash/conf/pyenv.bash
@@ -1,6 +1,7 @@
 export PYENV_ROOT=~/.pyenv
 export PATH="${PYENV_ROOT}/bin:${PATH}"
 
+if which pyenv > /dev/null; then eval "$(pyenv init --path)"; fi
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
